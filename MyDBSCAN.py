8d38a8d44caca4eefa658a70b7ebccc526802f5b
Elroye <elroye77@gmail.com>
2021-12-20 18:24:27 +0200

    get rid of unused functions

--- MyDBSCAN.py
+++ MyDBSCAN.py
@@ -59,37 +59,7 @@ class CMyDBSCAN:
                 if len(qNeighbors) >= self.minPoints:
                     seedSet.extend(qNeighbors)
         return self.clusters
-    
-    
-    def algoDBSCAN(self, dataSet):
-        for pIndex in dataSet:
-            if(pIndex in self.connectionsDictionary) == False:
-                continue
-            if(self.undefinedPoints[pIndex] == False):
-                continue
-    
-            neighbors = self.rangeQuery(dataSet, pIndex)
-            if len(neighbors) < self.minPoints:
-                self.noisePoints[pIndex] = True
-                self.undefinedPoints[pIndex] = False
-                continue
-            self.currnetCluster += 1 
-            self.clusters[pIndex] = self.currnetCluster
-            self.undefinedPoints[pIndex] = False
-            seedSet = neighbors[:]
-            for qIndex in seedSet:
-                if self.noisePoints[qIndex] == True:
-                    self.noisePoints[qIndex] = False
-                    self.clusters[qIndex] = self.currnetCluster
-                if self.undefinedPoints[qIndex] == False:
-                    continue
-                self.clusters[qIndex] = self.currnetCluster
-                self.undefinedPoints[qIndex] = False
-                qNeighbors = self.rangeQuery(dataSet, qIndex)
-                if len(qNeighbors) >= self.minPoints:
-                    seedSet.extend(qNeighbors)
-        return self.clusters
-    
+   
     
     
     def rangeQuery(self,data, qIndex):
@@ -97,18 +67,7 @@ class CMyDBSCAN:
             return [qIndex]
         return self.connectionsDictionary[qIndex]
     
-    
-
-    def calcEuclideanDistance(self,data, qIndex ,pIndex):
-        sm = 0
-        if(qIndex == pIndex):
-            return 0
-        p1 = data[qIndex]
-        p2 = data[pIndex]  
-        temp = p1 - p2
-        sm = np.dot(temp.T, temp) ** 0.5
-        return sm
-    
+       
     def dist(self, A):
         m, n = A.shape
         B = A @ A.T
@@ -158,18 +117,7 @@ class CMyDBSCAN:
         self.zipGrid()
         #elapsed = time.time() - t
         #print("zip grid time: ",elapsed)
-        
-    def connectNodes(self, key, pIndex, qIndex):
-        realPIndex = self.gridDictionaryIndexes[key][pIndex]
-        realQIndex = self.gridDictionaryIndexes[key][qIndex]
-        if (realPIndex in self.connectionsDictionary) == False:
-            self.connectionsDictionary.update({realPIndex : set()})
-        self.connectionsDictionary[realPIndex].add(realQIndex)
-        if (realQIndex in self.connectionsDictionary) == False:
-            self.connectionsDictionary.update({realQIndex : set()})
-        self.connectionsDictionary[realQIndex].add(realPIndex) 
-               
-        
+     
                 
     def initGraph(self, data):
         cnt = 0
